86fbd9c58ef9855df218ab507a6155c74faec9f7
cjy <jiayuncoder@163.com>
2024-09-19 11:22:18 +0800

    first commit

---
+++ src/main/java/org/yun/git/GitTest.java
@@ -0,0 +1,11 @@
+package org.yun.git;
+
+/**
+ * @author cjy
+ * @date 2024/9/19 11:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
